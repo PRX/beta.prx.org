94191273a95abee0edc27838b4eaa0f9f936796e
Rebecca Nesson <rebecca@prx.org>
2014-02-28 10:45:03 -0500

    getting data for displaying series

--- src/app/series/series.js
+++ src/app/series/series.js
@@ -1,4 +1,4 @@
-angular.module('prx.series', ['ui.router', 'angular-hal'])
+angular.module('prx.series', ['ui.router', 'angular-hal', 'prx.stories'])
 .config(function ($stateProvider, ngHalProvider, $urlRouterProvider) {
   $stateProvider.state('series', {
     url: '/series/:seriesId',
@@ -15,12 +15,16 @@ angular.module('prx.series', ['ui.router', 'angular-hal'])
   });
 
 ngHalProvider.setRootUrl(FEAT.apiServer)
-  .defineModule('http://meta.prx.org/model/series', [function () {
-    return {};
+  .mixin('http://meta.prx.org/model/series', ['resolved', function (resolved) {
+    resolved.$image = resolved.follow('image');
+  }])
+  .mixin('http://meta.prx.org/model/image', ['resolved', function (resolved) {
+    resolved.$imageUrl = resolved.call('link', 'enclosure').call('url');
   }]);
 })
 .controller('SeriesCtrl', function ($scope, series, stories) {
   $scope.series = series;
   $scope.stories = stories;
+  console.log($scope.series);
   console.log($scope.stories[0]);
 });
