89207c7ac7d51afc6e20a1246cd271688b9a5402
Rebecca Nesson <rebecca@prx.org>
2014-02-28 13:55:39 -0500

    fixes to series -- better follow syntax, no  used in view

--- src/app/series/series.js
+++ src/app/series/series.js
@@ -9,20 +9,21 @@ angular.module('prx.series', ['ui.router', 'angular-hal', 'prx.stories'])
         return ngHal.followOne('series', {id: $stateParams.seriesId});
       }],
       stories: ['series', function(series) {
-        return series.follow('stories').then(function(storiesDoc) { return storiesDoc.follow('items'); });
+        return series.follow('stories').follow('items');
       }]
     }
   });
 
 ngHalProvider.setRootUrl(FEAT.apiServer)
   .mixin('http://meta.prx.org/model/series', ['resolved', function (resolved) {
-    resolved.$image = resolved.follow('image');
+    resolved.image = resolved.follow('image');
   }])
-  .mixin('http://meta.prx.org/model/image/series', ['resolved', function (resolved) {
-    resolved.$imageUrl = resolved.call('link', 'enclosure').call('url');
+  .mixin('http://meta.prx.org/model/series_image', ['resolved', function (resolved) {
+    resolved.imageUrl = resolved.call('link', 'enclosure').call('url');
   }]);
 })
 .controller('SeriesCtrl', function ($scope, series, stories) {
   $scope.series = series;
   $scope.stories = stories;
+  console.log(series);
 });
