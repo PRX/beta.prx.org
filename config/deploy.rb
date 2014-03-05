7cbdf39448a534055d9ac0cb141bab401089b579
chrisrhoden <carhoden@gmail.com>
2014-03-05 17:06:50 -0500

    do not require agent forwarding

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,19 +2,12 @@
 lock '3.1.0'
 
 set :application, 'prx.org-frontend'
-set :repo_url, 'git@github.com:PRX/PRX.org-frontend.git'
+set :repo_url, 'git://github.com/PRX/PRX.org-frontend.git'
 set :deploy_to, '/var/www/domains/prx.org/m'
 set :linked_dirs, %w{node_modules vendor}
 set :default_env, { path: "/opt/node/current/bin:$PATH" }
 
 namespace :deploy do
-
-  before :starting, :start_ssh_agent do
-    run_locally do
-      %x(ssh-add)
-    end
-  end
-
   desc 'Compile assets'
   task :compile_assets do
     on roles(:web) do
