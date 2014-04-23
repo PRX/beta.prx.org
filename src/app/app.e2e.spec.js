2ea24819c5e8e427a9bedaf0a688a88dd2c5d7c9
chrisrhoden <carhoden@gmail.com>
2014-04-23 08:52:33 -0400

    fix for the logo tap taking you to the right place

---
+++ src/app/app.e2e.spec.js
@@ -0,0 +1,49 @@
+describe('application', function () {
+  describe('home page', function () {
+    beforeEach(function () {
+      browser.get('/');
+    });
+
+    it ('redirects to the /nxt modal', function () {
+      expect(browser.getCurrentUrl()).toMatch(/\/nxt$/);
+    });
+  });
+
+  describe('any page', function () {
+    beforeEach(function () {
+      browser.addMockModule('fakeStates', function () {
+        angular.module('fakeStates', ['ui.router'])
+        .config(function ($stateProvider) {
+          $stateProvider.state('fake', {
+            url: '/fake',
+            template: 'Hello! <a ui-sref=".modal">modal</a>'
+          }).state('fake.modal', {
+            url: '/modal',
+            views: {
+              'modal@': {
+                template: 'Hello!'
+              }
+            }
+          });
+        });
+      });
+
+      browser.get('/fake');
+    });
+
+    it ('takes you to the /nxt page when you tap the prx logo', function () {
+      $('h1 a').click();
+      expect(browser.getCurrentUrl()).toMatch(/\/nxt$/);
+    });
+
+    it ('opens modals', function () {
+      var modal = $('.modal');
+
+      expect(modal.isDisplayed()).toBe(false);
+      element(by.linkText('modal')).click();
+      expect(modal.isDisplayed()).toBe(true);
+      $('.dismiss').click();
+      expect(modal.isDisplayed()).toBe(false);
+    });
+  });
+});
