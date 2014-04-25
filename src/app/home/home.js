187a3b3cd7aad49e9a488007e39d2f3e6412483e
chrisrhoden <carhoden@gmail.com>
2014-04-25 16:14:13 -0400

    coverage

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,5 +1,7 @@
 angular.module('prx.home', ['ui.router', 'prx.home.storytime'])
 .config(function ($stateProvider, $urlRouterProvider) {
+
+  /* istanbul ignore else */
   if (!FEAT.HOME_PAGE) {
     $urlRouterProvider.when('/', '/nxt');
   }
@@ -15,6 +17,8 @@ angular.module('prx.home', ['ui.router', 'prx.home.storytime'])
     }
   });
 }).run(function ($rootScope, $state) {
+
+  /* istanbul ignore else */
   if (!FEAT.HOME_PAGE) {
     $rootScope.$on('$stateChangeStart', function (event, toState) {
       if (toState.name == 'home') {
--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -1,5 +1,10 @@
 describe('prx.home', function () {
 
   beforeEach(module('prx.home'));
-  
+
+  it ('redirects from home to home.nxt', inject(function ($rootScope, $state) {
+    spyOn($state, 'go');
+    $rootScope.$broadcast('$stateChangeStart', {name: 'home'});
+    expect($state.go).toHaveBeenCalled();
+  }));
 });
