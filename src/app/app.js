d8ca4a5c102cb9e7b4d754d8ee4f6f935e529e4a
chrisrhoden <carhoden@gmail.com>
2014-05-05 11:52:25 -0400

    Do not pollute A/B testing data with dev/integration behavior

--- src/app/app.js
+++ src/app/app.js
@@ -34,7 +34,7 @@ angular.module('prx', ['ngAnimate',
     } else {
       return 'tests';
     }
-  }]);
+  }]).enabled(FEAT.APPLICATION_VERSION != 'development' && FEAT.APPLICATION_VERSION != 'integration');
   ngFlagProvider.flags(FEAT.JSON);
 }).run(function ($rootScope, $location, $analytics, $timeout) {
   $rootScope.$on('$stateChangeSuccess', function () {
