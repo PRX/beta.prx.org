a6c73387fe29deb6b70f32226e05cc7730e8373c
Chris Kalafarski <chris@farski.com>
2014-07-14 12:54:04 -0400

    Updating series controller

--- src/app/series/series.js
+++ src/app/series/series.js
@@ -2,24 +2,24 @@ angular.module('prx.series', ['ui.router', 'angular-hal', 'prx.stories'])
 .config(function ($stateProvider, ngHalProvider) {
   $stateProvider.state('series', {
     url: '/series/:seriesId',
-    controller: 'SeriesCtrl',
+    controller: 'SeriesCtrl as series',
     templateUrl: 'series/series.html',
     resolve: {
       series: ['ngHal', '$stateParams', function (ngHal, $stateParams) {
-        return ngHal.followOne('series', {id: $stateParams.seriesId});
+        return ngHal.followOne('prx:series', {id: $stateParams.seriesId});
       }],
       stories: ['series', function(series) {
-        return series.follow('stories').follow('items');
+        return series.follow('prx:stories').follow('prx:items');
       }]
     }
   });
 
 ngHalProvider.setRootUrl(FEAT.apiServer)
   .mixin('http://meta.prx.org/model/series', ['resolved', function (resolved) {
-    resolved.imageUrl = resolved.follow('image').call('link', 'enclosure').call('url');
+    resolved.imageUrl = resolved.follow('prx:image').call('link', 'enclosure').call('url');
   }]);
 })
-.controller('SeriesCtrl', function ($scope, series, stories) {
-  $scope.series = series;
-  $scope.stories = stories;
+.controller('SeriesCtrl', function (series, stories) {
+  this.current = series;
+  this.stories = stories;
 });
