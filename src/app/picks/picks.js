7bcede33fd52c5681ae16de9fb229218468244df
Chris Rhoden <carhoden@gmail.com>
2014-08-14 10:26:31 -0400

    use that overflow watcher

--- src/app/picks/picks.js
+++ src/app/picks/picks.js
@@ -122,6 +122,28 @@ angular.module('prx.picks', ['prx.modelConfig'])
   }
 
 })
+.directive('overflowClass', function (OverflowCheck) {
+  return {
+    restrict: 'A',
+    link: function (scope, el, attrs) {
+      if(!attrs.overflowClass || attrs.overflowClass == 'overflow-class') {
+        attrs.overflowClass = 'overflowing';
+      }
+
+      OverflowCheck.watch(el[0], setClass);
+      scope.$on('$destroy', function () {
+        OverflowCheck.unwatch(el[0], setClass);
+      });
+      function setClass(on) {
+        if (on) {
+          el.addClass(attrs.overflowClass);
+        } else {
+          el.removeClass(attrs.overflowClass);
+        }
+      }
+    }
+  };
+})
 .directive('onOverflow', function (OverflowCheck) {
   return {
     restrict: 'A',
