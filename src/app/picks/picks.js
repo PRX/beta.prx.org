20af493275d6bc0919fdec87d9b0b232f87e7957
Chris Rhoden <carhoden@gmail.com>
2014-08-21 11:12:37 -0400

    Group picks without message together in list

--- src/app/picks/picks.js
+++ src/app/picks/picks.js
@@ -162,4 +162,26 @@ angular.module('prx.picks', ['prx.modelConfig'])
     }
   };
 })
+.filter('groupStandalonePicks', function () {
+  return function groupStandalonePicks(picks) {
+    var pick;
+    if (angular.isArray(picks)) {
+      for (var i=0; i<picks.length; i++) {
+        if (!picks[i].comment) {
+          for(var j=i+1; j<picks.length; j++) {
+            if (!picks[j].comment && j - 1 != i) {
+              pick = picks.splice(j, 1)[0];
+              picks.splice(i+1, 0, pick);
+              i = j;
+              break;
+            } else if (j+1 == picks.length) {
+              return picks;
+            }
+          }
+        }
+      }
+    }
+    return picks;
+  };
+})
 ;
