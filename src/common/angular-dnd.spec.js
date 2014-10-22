9e1e1b5387575f08bb16d18b2dec0781833ff8e5
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-10-22 16:37:33 -0400

    add real test

--- src/common/angular-dnd.spec.js
+++ src/common/angular-dnd.spec.js
@@ -1,2 +1,18 @@
 describe('angular-dnd', function () {
+  beforeEach(module('angular-dnd'));
+
+  describe('ngDrop', function() {
+
+    it('should get called on drop event', inject( function($rootScope, $compile) {
+      var element = $compile('<div ng-drop="dropped = true">')($rootScope);
+
+      expect($rootScope.dropped).not.toBeDefined();
+
+      element.triggerHandler('drop');
+      expect($rootScope.dropped).toBeDefined();
+
+    }));
+
+  });
+
 });
