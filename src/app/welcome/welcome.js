833d314173d1acd86845e8279244a7af984cdcb4
Chris Kalafarski <chris@farski.com>
2014-10-22 11:50:38 -0400

    Playback picks from Welcome

--- src/app/welcome/welcome.js
+++ src/app/welcome/welcome.js
@@ -1,20 +1,32 @@
-angular.module('prx.welcome', ['ngStorage'])
+angular.module('prx.welcome', ['ngStorage', 'prx.player', 'prx.home'])
 .directive('prxWelcome', function () {
   return {
     restrict: 'E',
     replace: true,
     controller: 'WelcomeCtrl',
     controllerAs: 'welcome',
-    templateUrl: 'welcome/welcome.html'
+    templateUrl: 'welcome/welcome.html',
+    scope: { picks: '=' }
   };
 })
 .service('prxWelcome', function () {
   this.visible = false;
 })
-.controller('WelcomeCtrl', function (prxWelcome, $scope, $localStorage) {
+.controller('WelcomeCtrl', function (prxWelcome, $scope, $localStorage, prxPlayer, prxSoundFactory) {
   $scope.$storage = $localStorage;
+
+  var sp = $scope.picks[0].story.toSoundParams();
 
   this.visible = function () {
     return !$localStorage.welcomed;
+  };
+
+  this.play = function () {
+    sp.then(function (sp) {
+      var event = $scope.$emit('$play', sp);
+      if (!event.defaultPrevented) {
+        prxPlayer.play(prxSoundFactory(sp));
+      }
+    });
   };
 });
