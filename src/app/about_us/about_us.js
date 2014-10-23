77bbd5e518c2989b1fe3371283363fc0528cc4c5
Eve Asher <eve.adele.asher@gmail.com>
2014-10-23 14:24:54 -0400

    add funding page

---
+++ src/app/about_us/about_us.js
@@ -0,0 +1,24 @@
+angular.module('prx.aboutus', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('aboutUs', {
+    abstract: true,
+    title: 'About Us'
+  })
+  .state('aboutUs.whatIsPrx', {
+    url: '/about-us/what-is-prx',
+    views: {
+      '@': {
+        templateUrl: 'about_us/what_is_prx.html'
+      }
+    },
+    title: 'What Is PRX'
+  })
+  .state('aboutUs.funding', {
+    url: '/about-us/funding',
+    views: {
+      '@': {
+        templateUrl: 'about_us/funding.html'
+      }
+    }
+  });
+});
