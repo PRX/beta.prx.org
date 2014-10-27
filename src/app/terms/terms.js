0ff68347726fe23651cf448fd754c15f995801ba
Eve Asher <eve.adele.asher@gmail.com>
2014-10-27 15:13:41 -0400

    make user agreement page

--- src/app/terms/terms.js
+++ src/app/terms/terms.js
@@ -1,11 +1,20 @@
 angular.module('prx.terms', ['ui.router'])
 .config(function ($stateProvider) {
   $stateProvider.state('terms', {
-    url: '/terms',
+    url: '/terms-of-use',
     title: 'Terms of Use',
     views: {
       '@': {
         templateUrl: 'terms/terms.html'
+      }
+    }
+  })
+  .state('terms.userAgreement', {
+    url: '/user-agreement',
+    title: 'User Agreement',
+    views: {
+      '@': {
+        templateUrl: 'terms/user_agreement.html'
       }
     }
   });
