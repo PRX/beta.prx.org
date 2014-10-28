34190588f8cefbeabd6f5a1942d2cfdf1f47bf9b
Chris Kalafarski <chris@farski.com>
2014-10-28 15:42:51 -0400

    Sticky elements

---
+++ src/app/upload/upload.js
@@ -0,0 +1,281 @@
+angular.module('prx.upload', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('upload', {
+      url: '/prx-upload',
+      title: 'Create Your Story',
+      views: {
+        '@': {
+          templateUrl: 'upload/upload.html',
+          controller: 'UploadCtrl as upload'
+        }
+      }
+    }
+  );
+})
+.controller('UploadCtrl', function () {
+})
+.directive('onPageScroll', function ($window) {
+  return {
+    restrict: 'A',
+    controller: function () {
+      this.root = null;
+
+      this.statusBar = undefined;
+      this.statusBarPlaceholder = undefined;
+
+      this.inspectors = [];
+      this.inspectorPlaceholders = [];
+
+
+      this.registerStatusBar = function (elem) {
+        this.statusBar = elem;
+      };
+      this.registerStatusBarPlaceholder = function (elem) {
+        this.statusBarPlaceholder = elem;
+      };
+      this.registerInspector = function (elem) {
+        this.inspectors.push(elem);
+      };
+      this.registerInspectorPlaceholder = function (elem) {
+        this.inspectorPlaceholders.push(elem);
+      };
+
+      this.findPosX = function (obj) {
+        var curleft = 0;
+
+        if(obj.offsetParent) {
+          while(1) {
+            curleft += obj.offsetLeft;
+
+            if(!obj.offsetParent) {
+              break;
+            }
+
+            obj = obj.offsetParent;
+          }
+        } else if (obj.x) {
+          curleft += obj.x;
+        }
+
+        return curleft;
+      };
+
+      this.findPosY = function (obj) {
+        var curtop = 0;
+
+        if (obj.offsetParent) {
+          while(1) {
+            curtop += obj.offsetTop;
+
+            if(!obj.offsetParent) {
+              break;
+            }
+
+            obj = obj.offsetParent;
+          }
+        } else if (obj.y) {
+          curtop += obj.y;
+        }
+
+        return curtop;
+      };
+
+      this.positionInspectors = function () {
+        for (i = 0; i < this.inspectors.length; i++) {
+
+          _placeholder = this.inspectorPlaceholders[i][0];
+          _inspector = this.inspectors[i][0];
+
+          _nextInspector = undefined;
+          if (this.inspectors[i + 1]) {
+            _nextInspector = this.inspectors[i + 1][0];
+          }
+
+          pageY = $window.pageYOffset;
+
+          verticalOffset = 153;
+          topMargin = 30;
+
+          y = this.findPosY(_placeholder);
+
+          if (pageY + verticalOffset > y) {
+            // Has scrolled past the static location of the inspector,
+            // so we need to figure out where to display it
+
+            // Figure out if there's collision between the display location of
+            // this inspector and the static location of the next inspector
+
+            if ((typeof _nextInspector != 'undefined')) {
+              nextY = this.findPosY(_nextInspector);
+
+              collisionY = nextY - (verticalOffset + topMargin + _inspector.offsetHeight);
+              adjCollisionY = (collisionY - 208);
+
+              if (pageY > adjCollisionY) {
+                // Push the inspector off screen as necessary
+
+                distPastCollisionY = (pageY - adjCollisionY);
+                _y = (verticalOffset + topMargin) - distPastCollisionY;
+
+                _inspector.style.position = "fixed";
+                _inspector.style.top = _y + "px";
+                _inspector.style.left = 'initial';
+                _inspector.style.marginLeft = "-255px";
+              } else {
+                // Pin the current header to the top of the view
+                _inspector.style.position = "fixed";
+                _inspector.style.top = (verticalOffset + topMargin) + "px";
+                _inspector.style.left = 'initial';
+                _inspector.style.marginLeft = "-255px";
+              }
+
+              // pushY = ();
+
+
+            } else {
+              // Pin the current header to the top of the view
+              _inspector.style.position = "fixed";
+              _inspector.style.top = (verticalOffset + topMargin) + "px";
+              _inspector.style.left = 'initial';
+              _inspector.style.marginLeft = "-255px";
+            }
+          } else {
+            // Reset the inspector's styles
+            _inspector.style.removeProperty('marginLeft');
+            _inspector.style.removeProperty('margin');
+            _inspector.style.removeProperty('position');
+            _inspector.style.removeProperty('top');
+            _inspector.style.removeProperty('left');
+          }
+
+
+
+
+
+
+
+
+        //   // Placeholder for the current header has been scrolled out the top
+        //   // (ie we are scrolled past the natural header position)
+        //   // need to account for the header (73px)
+        //   if ($window.pageYOffset + 153 > this.findPosY(_placeholder)) {
+        //
+        //     // There are more headers after this one
+        //     if (typeof _nextInspector != 'undefined'){
+        //
+        //       nextMinY = this.findPosY(_nextInspector);
+        //       adjNextMinY = nextMinY - 550;
+        //
+        //       breakpoint = adjNextMinY;
+        //
+        //       // The distance between where the page is currenly scrolled to and
+        //       // the natural position of the next header
+        //       dif = this.findPosY(_nextInspector) - $window.pageYOffset;
+        //
+        //       // if ((dif) < _inspector.offsetHeight) {
+        //       if ($window.pageYOffset > breakpoint) {
+        //         console.log('push');
+        //         // The next header is pushing the current header off screen
+        //         _inspector.style.position = "fixed";
+        //         _inspector.style.top = '-' + (_inspector.offsetHeight - dif - 200) + 'px';
+        //         _inspector.style.left = 'initial';
+        //         _inspector.style.marginLeft = "-255px";
+        //       } else {
+        //         // Pin the current header to the top of the view
+        //         _inspector.style.position = "fixed";
+        //         _inspector.style.top = "182px";
+        //         _inspector.style.left = 'initial';
+        //         _inspector.style.marginLeft = "-255px";
+        //       }
+        //     } else {
+        //       // There are no headers after this one
+        //       // _placeholder.style.height = _inspector.offsetHeight + 'px';
+        //       _inspector.style.position = "fixed";
+        //       _inspector.style.top = "182px";
+        //       _inspector.style.left = 'initial';
+        //       _inspector.style.marginLeft = "-255px";
+        //     }
+        //   } else {
+        //     // Natural location for this header is below the top of the page
+        //     _inspector.style.removeProperty('marginLeft');
+        //     _inspector.style.removeProperty('margin');
+        //     _inspector.style.removeProperty('position');
+        //     _inspector.style.removeProperty('top');
+        //     _inspector.style.removeProperty('left');
+        //   }
+
+
+
+
+
+
+
+
+
+
+        }
+      };
+
+      this.positionStatusBar = function () {
+        bar = this.statusBar[0];
+        placeholder = this.statusBarPlaceholder[0];
+
+        if ($window.pageYOffset + 73 > this.findPosY(placeholder)) {
+          placeholder.style.height = bar.offsetHeight + 'px';
+          bar.style.position = "fixed";
+          bar.style.top = "73px";
+        } else {
+          placeholder.style.height = '0px';
+          bar.style.position = 'static';
+          bar.style.removeProperty('top');
+        }
+      };
+    },
+    link: function (scope, elem, attrs, ctrl) {
+      ctrl.root = elem[0];
+
+      angular.element($window).on('scroll', function (event) {
+        ctrl.positionStatusBar();
+        ctrl.positionInspectors();
+      });
+    }
+  };
+})
+.directive('statusBar', function () {
+  return {
+    restrict: 'A',
+    require: '^onPageScroll',
+    link: function (scope, elem, attrs, ctrl) {
+      ctrl.registerStatusBar(elem);
+    }
+  };
+})
+.directive('statusBarPlaceholder', function () {
+  return {
+    restrict: 'A',
+    require: '^onPageScroll',
+    link: function (scope, elem, attrs, ctrl) {
+      ctrl.registerStatusBarPlaceholder(elem);
+    }
+  };
+})
+.directive('inspector', function () {
+  return {
+    restrict: 'A',
+    require: '^onPageScroll',
+    link: function (scope, elem, attrs, ctrl) {
+      ctrl.registerInspector(elem);
+    }
+  };
+})
+.directive('inspectorPlaceholder', function () {
+  return {
+    restrict: 'A',
+    require: '^onPageScroll',
+    link: function (scope, elem, attrs, ctrl) {
+      ctrl.registerInspectorPlaceholder(elem);
+    }
+  };
+})
+;
