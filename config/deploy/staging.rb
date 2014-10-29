0e7eb09b667c4a84939bf8d1f8acbea547777a69
Chris Rhoden <carhoden@gmail.com>
2014-10-29 12:34:14 -0400

    flag the welcome mat

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,4 @@ role :web, %w{deploy@staging.m.prx.org}
 set :deploy_to, '/var/www/domains/prx.org/m.staging'
 ask :branch_name, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :branch, proc { fetch(:branch_name); `git rev-parse #{fetch(:branch_name)}`.chomp }
+set :default_env, { welcome_mat: true, path: "/opt/node/current/bin:/opt/python/current/bin:$PATH" }
