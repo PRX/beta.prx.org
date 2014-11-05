5c220839c89de4dbed81fd400fd5a7d9bc5baed7
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-11-05 10:16:27 -0500

    flatten tags into result

--- src/common/angular-id3.js
+++ src/common/angular-id3.js
@@ -6,13 +6,26 @@ angular.module('angular-id3', [])
 })
 .service('Id3Service', function (id3, $rootScope, $q) {
 
+  var flattenTags = function (tags) {
+    var ft = angular.copy(tags);
+    for (var i=0; i<=3; i++) {
+      var vtags = ft['v'+i];
+      if (angular.isDefined(vtags)) {
+        angular.extend(ft, vtags);
+        delete ft['v'+i];
+      }
+    }
+    return ft;
+  };
+
   this.analyze = function (file) {
     var deferred = $q.defer();
-    id3(file, function(err, tags){
+    id3(file, function(err, tags) {
 
       $rootScope.$evalAsync( function() {
-        if (tags) {
-          deferred.resolve(tags);
+        if (angular.isDefined(tags)) {
+          var flat = flattenTags(tags);
+          deferred.resolve(flat);
         } else {
           deferred.reject(err);
         }
