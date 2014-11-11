31b3790e01b3ee0bad5c878cea4fa2dc790e4bc3
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-11-11 12:25:12 -0500

    add aurora service and spec

---
+++ src/common/angular-aurora-spec.js
@@ -0,0 +1,69 @@
+describe('angular-aurora', function () {
+
+  describe ('$AV', function () {
+
+    beforeEach(module('angular-aurora'));
+
+    beforeEach(inject(function (_$AV_, _AuroraService_) {
+      $AV = _$AV_;
+      AuroraService = _AuroraService_;
+    }));
+
+    it ('provides the aurora AV object', function () {
+      expect($AV).toBeDefined();
+      expect($AV.Asset).toBeDefined();
+    });
+
+  });
+
+  describe ('AuroraService', function() {
+    var $AV, AuroraService, $rootScope;
+
+    beforeEach(function() {
+      module('angular-aurora', function ($provide) {
+
+        $provide.decorator('$window', ['$delegate', function($delegate) {
+          var MockAsset = function (file) { this.file = file; };
+          MockAsset.fromFile = function(file) { return new MockAsset(file); };
+          MockAsset.prototype = {
+            get: function(g, callback) {
+              callback(this.file['mockGet']);
+            }
+          };
+
+          var MockAV  =function () {};
+          MockAV.Asset = MockAsset;
+          $delegate.AV = MockAV;
+          return $delegate;
+        }]);
+
+      });
+    });
+
+    beforeEach(inject(function (_AuroraService_, _$AV_, _$rootScope_) {
+      AuroraService = _AuroraService_;
+      $AV = _$AV_;
+      $rootScope = _$rootScope_;
+    }));
+
+    it ("will analyze file format", function() {
+      expect(AuroraService).toBeDefined();
+      var mockFile = {name: 'test.mp3', type: 'audio/mpeg', mockGet: {bitRate: '128'} };
+      AuroraService.format(mockFile).then( function(res) {
+        expect(res['bitRate']).toEqual('128');
+      });
+      $rootScope.$digest();
+    });
+
+    it ("can get duration of a file", function() {
+      expect(AuroraService).toBeDefined();
+      var mockFile = {name: 'test.mp3', type: 'audio/mpeg', mockGet: 120 };
+      AuroraService.duration(mockFile).then( function(res) {
+        expect(res).toEqual(120);
+      });
+      $rootScope.$digest();
+    });
+
+  });
+
+});
---
+++ src/common/angular-aurora.js
@@ -0,0 +1,40 @@
+angular.module('angular-aurora', [])
+.factory('$AV', [ '$window', function ($window) {
+  return $window.AV;
+}])
+.service('AuroraService', function ($AV, $rootScope, $q) {
+
+  // formatID is less than useful, update it
+  var correctFormat = function (format) {
+    var cf = angular.copy(format);
+    return cf;
+  };
+
+  var getInfo = function (file, info) {
+    var deferred = $q.defer();
+    var asset = $AV.Asset.fromFile(file);
+
+    asset.get(info, function (res) {
+      $rootScope.$evalAsync( function() {
+        deferred.resolve(res);
+      });
+    });
+
+    return deferred.promise;
+  };
+
+  this.format = function (file) {
+    return getInfo(file, 'format').then( function (format) {
+      return correctFormat(format);
+    });
+  };
+
+  this.duration = function (file) {
+    return getInfo(file, 'duration');
+  };
+
+  this.metadata = function (file) {
+    return getInfo(file, 'metadata');
+  };
+
+});
