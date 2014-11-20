42509dd8044334c67b104baaa39e8e208dc91275
Chris Rhoden <carhoden@gmail.com>
2014-11-20 15:22:02 -0500

    fix deploy scripts and stop running protractor in travis

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-role :web, %w{deploy@m.prx.org}
+role :web, %w{deploy@beta.prx.org}
 set :deploy_to, '/var/www/domains/prx.org/m'
 set :branch, :master
 set :default_env, { path: "/opt/node/current/bin:/opt/python/current/bin:$PATH" }
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-role :web, %w{deploy@staging.m.prx.org}
+role :web, %w{deploy@alpha.prx.org}
 set :deploy_to, '/var/www/domains/prx.org/m.staging'
 ask :branch_name, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :branch, proc { fetch(:branch_name); `git rev-parse #{fetch(:branch_name)}`.chomp }
