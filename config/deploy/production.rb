975a4baf8734c434fbcff9d2e88b4e2038c2a457
Christopher Kalafarski <farski@Mideel.cambridge.prx>
2015-01-08 17:03:06 -0500

    Make Hound happy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,12 @@ set :deploy_to, '/var/www/domains/prx.org/m'
 set :branch, :master
 set :default_env, { path: '/opt/node/current/bin:/opt/python/current/bin:$PATH' }
 
-set :slack_webhook, -> { _hook = ''; on(roles(:web)) { _hook = capture('cat /var/www/domains/prx.org/hal/shared/config/slack_webhook.txt') }; _hook }
+set :slack_webhook, -> {
+  webhook = nil
+  on roles(:web) do
+    cmd = 'cat /var/www/domains/prx.org/hal/shared/config/slack_webhook.txt'
+    webhook = capture cmd
+  end
+  webhook
+}
 set :slack_username, -> { 'capistrano' }
