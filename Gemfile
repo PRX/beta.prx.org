accd4e2033e603493f43baf9f27f3b517537c725
Christopher Kalafarski <farski@Mideel.cambridge.prx>
2015-01-08 15:13:16 -0500

    Add notifications with Slackistrano

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'capistrano', '~> 3.1.0'
+gem 'slackistrano', require: false
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
 role :web, %w{deploy@beta.prx.org}
 set :deploy_to, '/var/www/domains/prx.org/m'
 set :branch, :master
-set :default_env, { path: "/opt/node/current/bin:/opt/python/current/bin:$PATH" }
+set :default_env, { path: '/opt/node/current/bin:/opt/python/current/bin:$PATH' }
+
+set :slack_webhook, 'https://hooks.slack.com/services/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'
+set :slack_username, -> { 'capistrano' }
