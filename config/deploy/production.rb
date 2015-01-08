473f7f25da9e06cd7646de312e84ea6148260c9d
Christopher Kalafarski <farski@Mideel.cambridge.prx>
2015-01-08 16:45:35 -0500

    Remove Slack webhook URL from version control

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ set :deploy_to, '/var/www/domains/prx.org/m'
 set :branch, :master
 set :default_env, { path: '/opt/node/current/bin:/opt/python/current/bin:$PATH' }
 
-set :slack_webhook, 'https://hooks.slack.com/services/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'
+set :slack_webhook, -> { _hook = ''; on(roles(:web)) { _hook = capture('cat /var/www/domains/prx.org/hal/shared/config/slack_webhook.txt') }; _hook }
 set :slack_username, -> { 'capistrano' }
