2514dcd4874797d9031069e2239058d907ce9cdb
Eve Asher <eve.adele.asher@gmail.com>
2015-02-24 10:50:09 -0500

    WIP create filepicker service

--- src/app/upload/filepicker/filepicker.js
+++ src/app/upload/filepicker/filepicker.js
@@ -19,6 +19,65 @@ angular.module('prx.upload.filepicker', ['templates'])
     replace: true
   };
 })
-.controller('FilePickerOverlayCtrl', function (PRXFilePicker) {
+.controller('FilePickerOverlayCtrl', function (PRXFilePicker, Upload) {
   this.picker = PRXFilePicker;
+  this.filesHovering = function (event) {
+    event.preventDefault();
+    this.hovering = true;
+  };
+
+  this.filesLeave = function (event) {
+    event.preventDefault();
+    this.hovering = false;
+  };
+
+  this.filesDropped = function (event) {
+    event.preventDefault();
+    this.hovering = false;
+    this.selectFiles(event.dataTransfer.files);
+  };
+
+  this.selectFiles = function (files) {
+    angular.forEach(files, function (file) {
+      Upload.upload(file).then(function () {
+        console.log(arguments);
+      }, function () {
+        console.log(arguments);
+      }, function () {
+        console.log(arguments);
+      });
+    });
+  };
+})
+.directive('prxFileSelectButton', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, elem, attrs) {
+      var input = angular.element("<input type='file'>");
+      elem.on("click", openFileInput);
+      scope.$on("$destroy", function () {
+        elem.off("click", openFileInput);
+        input.off('change', filesSelected);
+      });
+
+      input.on('change', filesSelected);
+
+      function openFileInput(e) {
+        e.stopPropagation();
+        input[0].click();
+      }
+
+      function filesSelected(e) {
+        scope.$eval(attrs.onFilesSelected, {'$files': input[0].files});
+      }
+    }
+  };
+})
+.directive('prxLocalFileUploadTab', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'upload/filepicker/tabs/local-files.html',
+    replace: true,
+    scope: false
+  };
 });
