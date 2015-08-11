85b4c604a2aac71777ff9e01dcc39fe4d9408279
Chris Rhoden <carhoden@gmail.com>
2015-08-11 14:15:01 -0400

    improve staging deploys

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,20 +1,23 @@
+require 'json'
 role :web, %w{deploy@alpha.prx.org}
 set :deploy_to, '/var/www/domains/prx.org/m.staging'
-ask :branch_name, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :branch, proc { fetch(:branch_name); `git rev-parse #{fetch(:branch_name)}`.chomp }
+set :branch, :master
 
-set :default_env, {
-  welcome_mat: true,
-  tcf_demo: true,
-  id_client_key: 'X5rYnqHGVBpqPTfHKYQyxG6PYfVj0l8nostkQzTC',
-  path: '/opt/node/current/bin:/opt/python/current/bin:$PATH' }
+set :default_env, -> {
+  filename = File.expand_path('../../flags.staging.json' , __FILE__)
+  json = JSON.parse(File.read(filename))
+  Hash[json.map{|(k,v)| [k.downcase.intern, v]}].merge( {
+    path: '/opt/node/current/bin:/opt/python/current/bin:$PATH'
+  })
+}
 
-  set :slack_webhook, -> {
-    webhook = nil
-    on roles(:web) do
-      cmd = "cat #{fetch(:deploy_to)}/shared/config/slack_webhook.txt"
-      webhook = capture cmd
-    end
-    webhook
-  }
-  set :slack_username, -> { 'capistrano' }
+
+set :slack_webhook, -> {
+  webhook = nil
+  on roles(:web) do
+    cmd = "cat #{fetch(:deploy_to)}/shared/config/slack_webhook.txt"
+    webhook = capture cmd
+  end
+  webhook
+}
+set :slack_username, -> { 'capistrano' }
