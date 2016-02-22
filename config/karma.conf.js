0bea79b35f9f932532a58315b582a6d5eec0bc72
cavis <ryancavis@gmail.com>
2016-02-22 11:29:31 -0700

    Fix unit-coverage reporting and failing

---
+++ config/karma.conf.js
@@ -0,0 +1,86 @@
+/**
+ * Karma unit testing configs
+ */
+module.exports = function(config) {
+  var files = ['src/**/*.spec.js'];
+
+  // optional "--file" globs
+  if (process.argv.indexOf('--file') > -1) {
+    files = [];
+    for (var i = 0; i < process.argv.length; i++) {
+      if (process.argv[i] == '--file' && process.argv[i+1]) {
+        files.push(process.argv[i+1]);
+      }
+    }
+  }
+
+  // preprocess for browserify and coverage
+  var preprocessors = {};
+  files.forEach(function (file) {
+    preprocessors[file] = ['browserify', 'coverage'];
+  });
+
+  // TODO: this is a bit hacky
+  files.push('build/assets/templates.js');
+
+  // prevent annoying 404 errors
+  files.push({pattern: 'src/assets/images/*', watched: false, included: false, served: true});
+
+  config.set({
+    basePath: '../',
+    urlRoot: '/',
+    autoWatch: false,
+
+    frameworks: ['browserify', 'jasmine'],
+    plugins:    [
+      'karma-browserify',
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-chrome-launcher'
+      // 'karma-firefox-launcher',
+      // 'karma-safari-launcher',
+      // 'karma-phantomjs-launcher',
+    ],
+
+    browsers:   ['Chrome'],
+
+    files: files,
+    preprocessors: preprocessors,
+    proxies: {
+      '/assets/images/': '/base/src/assets/images/'
+    },
+    exclude: [ '**/*.e2e.spec.js' ],
+
+    browserify: {
+      fullpaths: true,
+      debug: true,
+      builtins: [/* don't need 'em */],
+      transform: [
+        ['browserify-istanbul', {
+          ignore: ['**/*.spec.js'],
+          // TODO: https://github.com/karma-runner/karma-coverage/issues/157#issuecomment-160555004
+          instrumenterConfig: { embedSource: true }
+        }],
+      ]
+    },
+
+    reporters: ['dots', 'coverage'],
+    coverageReporter: {
+      check: {
+        global: {
+          statements: 70,
+          branches:   70,
+          functions:  70,
+          lines:      70
+        }
+      },
+      reporters: [
+        {type: 'text-summary'},
+        {type: 'html', dir: 'coverage'},
+        {type: 'json', dir: 'coverage'},
+        {type: 'lcov', dir: 'coverage'}
+      ]
+    }
+
+  });
+};
--- gulp/specunit.js
+++ gulp/specunit.js
@@ -5,30 +5,7 @@ var gutil = require('gulp-util');
  * Run jasmine specs via karma
  */
 module.exports = function (gulp) {
-  var files = ['src/**/*.spec.js'];
 
-  // optional "--file" globs
-  if (process.argv.indexOf('--file') > -1) {
-    files = [];
-    for (var i = 0; i < process.argv.length; i++) {
-      if (process.argv[i] == '--file' && process.argv[i+1]) {
-        files.push(process.argv[i+1]);
-      }
-    }
-  }
-
-  var preprocessors = {};
-  files.forEach(function (file) {
-    preprocessors[file] = ['browserify'];
-  });
-
-  // TODO: this is hacky
-  files.push('build/assets/templates.js');
-
-  // prevent annoying 404 errors
-  files.push({pattern: 'src/assets/images/*', watched: false, included: false, served: true});
-
-  // TODO: move back into karma.conf.js
   return function (done) {
 
     // karma exit codes don't work with gulp
@@ -40,23 +17,16 @@ module.exports = function (gulp) {
       }
       else {
         done();
+        // TODO: the text-summary reporter seems to hang here!
+        process.nextTick(function() {
+          process.exit(0);
+        })
       }
     }
 
     new karma.Server({
       singleRun: true,
-      config: { basePath: '../' },
-      proxies: {
-        '/assets/images/': '/base/src/assets/images/'
-      },
-      frameworks: [ 'browserify', 'jasmine' ],
-      plugins:    [ 'karma-browserify', 'karma-jasmine', 'karma-chrome-launcher' ],
-      browsers:   ['Chrome'],
-      files: files,
-      exclude: [ '**/*.e2e.spec.js' ],
-      preprocessors: preprocessors,
-      browserify: { fullpaths: true, debug: true, builtins: [/* don't need 'em */] },
-      reporters: [ 'dots' ]
+      configFile: __dirname + '/../config/karma.conf.js'
     }, handleKarmaExit).start();
 
   };
