d6167415aafd6b877092966bfda169da2ec467ce
cavis <ryancavis@gmail.com>
2016-03-02 09:43:16 -0700

    Errrr... maybe try phantom then?

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -21,16 +21,18 @@ if (process.env['SAUCE_USERNAME'] && process.env['SAUCE_ACCESS_KEY']) {
     'tunnel-identifier': (process.env['TRAVIS_JOB_NUMBER'] || null)
   }];
 }
+else if (process.env['SNAP_CI']) {
+  // TODO: chromedriver doesn't want to work here
+  // exports.config.directConnect = true;
+  // exports.config.chromeDriver = '/usr/local/bin/chromedriver';
+  exports.config.jasmineNodeOpts.defaultTimeoutInterval = 10000;
+  exports.config.capabilities = {
+    'browserName': 'phantomjs'
+  };
+}
 else {
   exports.config.jasmineNodeOpts.defaultTimeoutInterval = 10000;
   exports.config.capabilities = {
     'browserName': 'chrome'
   };
-
-  // get the correct chromedriver in snap
-  if (process.env['SNAP_CI']) {
-    console.log('Running snap-ci protractor via chromedriver...');
-    exports.config.directConnect = true;
-    exports.config.chromeDriver = '/usr/local/bin/chromedriver';
-  }
 }
